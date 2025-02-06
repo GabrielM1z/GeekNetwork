a7b70470f64f7aa0e8b6e611b4d8cbd8f78be039
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2025-02-06 11:32:50 +0100

    avant modif route

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,6 @@
 const pool = require('../config/dbPostgres');
 
-exports.getUsers = async (req, res) => {
+exports.getUsersSQL = async (req, res) => {
     try {
         const { rows } = await pool.query('SELECT * FROM users LIMIT 10');
         res.json(rows);
