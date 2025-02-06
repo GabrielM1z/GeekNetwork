db82322ca26d88510f6de817f6be31ce62f6d1a8
GabrielM1z <gabazoo.martinez@gmail.com>
2025-02-06 14:42:53 +0100

    close seesison fix

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 // Fonction pour récupérer les utilisateurs depuis PostgreSQL
 const getUsersSQL = (res) => {
   const query = "SELECT * FROM users LIMIT 10"; // Exemple de requête PostgreSQL
-  
+
   pool.query(query)
     .then(result => {
       res.json({ users: result.rows }); // Renvoie les utilisateurs récupérés
@@ -29,27 +29,27 @@ const driver = neo4j.driver(
   process.env.NEO4J_URI,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 );
-const session = driver.session();
 
 // Fonction pour récupérer les utilisateurs depuis Neo4j
 const getUsersNeo4j = (res) => {
-    const query = "MATCH (u:User) RETURN u LIMIT 10"; // Exemple de requête Neo4j
-    
-    session.run(query)
-        .then(result => {
-        const users = result.records.map(record => record.get("u").properties);
-        res.json({ users }); // Renvoie les utilisateurs récupérés depuis Neo4j
-        })
-        .catch(err => {
-        console.error("Erreur Neo4j:", err);
-        res.status(500).json({ error: "Erreur de récupération des utilisateurs" });
-        })
-        .finally(() => {
-        session.close();
-        });
+  const session = driver.session();
+  const query = "MATCH (u:User) RETURN u LIMIT 10"; // Exemple de requête Neo4j
+
+  session.run(query)
+    .then(result => {
+      const users = result.records.map(record => record.get("u").properties);
+      res.json({ users }); // Renvoie les utilisateurs récupérés depuis Neo4j
+    })
+    .catch(err => {
+      console.error("Erreur Neo4j:", err);
+      res.status(500).json({ error: "Erreur de récupération des utilisateurs" });
+    })
+    .finally(() => {
+      session.close();
+    });
 };
 
 module.exports = {
-    getUsersSQL,
-    getUsersNeo4j,
+  getUsersSQL,
+  getUsersNeo4j,
 };
